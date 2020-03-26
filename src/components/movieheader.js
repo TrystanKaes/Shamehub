83ebe082ff986491fc7f8abac3c22eb512fbfb4a
Anthony Dupont <Frenchy2199@yahoo.com>
2020-03-26 14:22:59 -0600

    Changing the name from movie app to shamehub, it's a test

--- src/components/movieheader.js
+++ src/components/movieheader.js
@@ -17,7 +17,7 @@ class MovieHeader extends Component {
                 <Navbar>
                     <Navbar.Header>
                         <Navbar.Brand>
-                            Movie App
+                            ShameHub
                         </Navbar.Brand>
                     </Navbar.Header>
                     <Nav>
