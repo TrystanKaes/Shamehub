21ec6d75d1bceaa6f3db0695822265897a73fb1c
Anthony Dupont <Frenchy2199@yahoo.com>
2020-03-26 21:38:34 -0600

    WOW HAS IT BEEN A FUCKING JOURNEY, made it so that for every failed login attempts, you get insulted. A bit of it is hard coded right now, waiting to fix the backend to remove the hardcoded parts

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -14,8 +14,39 @@ function logout(){
     }
 }
 
+export function getInsult(data){
+    const env = runtimeEnv();
+    console.log(JSON.stringify(data));
+    //return dispatch => {
+        return fetch(`${env.REACT_APP_API_URL}/insults`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            //body: JSON.stringify(data),   //This was causing issues, apperently you cannot send a body for a GET request
+            mode: 'cors'})
+            .then( (response) => {
+                console.log("Inside the first then in insult");
+                if (!response.ok) {
+                    // we failed to retrieve an insult
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then( (res) => {
+                // The promise above returns to us the response in json format, now we want to send back an insult
+                console.log("Inside the second then for insults");
+                return res.insults; //TODO make this more specific once we can specify the category in the headers instead of body
+            })
+            .catch( (e) => console.log(e) );
+    //}
+}
+
+
 export function submitLogin(data){
     const env = runtimeEnv();
+    console.log(JSON.stringify(data));
     return dispatch => {
         return fetch(`${env.REACT_APP_API_URL}/signin`, {
             method: 'POST',
@@ -26,16 +57,27 @@ export function submitLogin(data){
             body: JSON.stringify(data),
             mode: 'cors'})
             .then( (response) => {
-                if (!response.ok) {
-                    throw Error(response.statusText);
-                }
                 return response.json();
             })
             .then( (res) => {
-                localStorage.setItem('username', data.username);
-                localStorage.setItem('token', res.token);
+                // The promise above returns to us the response in json format, now we want to check if we've logged in or not
+                if(res.success === "true") {
+                    localStorage.setItem('username', data.username);
+                    localStorage.setItem('token', res.token);
 
-                dispatch(userLoggedIn(data.username));
+                    dispatch(userLoggedIn(data.username));
+                }
+                else{
+                    // we didn't log in, display an insult
+                    var data_to_send = {"category": "incorrect password"};  //TODO fix this to not be hardcoded once this is fixed in the backend
+                    return getInsult(data_to_send);
+                }
+            })
+            .then( (insult) => {
+                //Here we are inside another god damn .then JAVASCRIPT YOU ARE FUCKING SOMETHING ALRIGHT
+                var i = Math.floor(Math.random() * 4);
+                var random = insult[i].insult;  //TODO fix this to not be from random and be specific once I can send the category through headers instead of body (backend)
+                alert(random);
             })
             .catch( (e) => console.log(e) );
     }
