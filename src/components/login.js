b6af652e0aa597563aaccaf1109285ec83ddd891
Anthony Dupont <Frenchy2199@yahoo.com>
2020-04-17 23:29:08 -0600

    Made the bypass change the state immutably instead

--- src/components/login.js
+++ src/components/login.js
@@ -45,8 +45,15 @@ class Login extends Component {
 
     bypassLogin(){
         //username and password stored in plane text aaaahhh!!!, use env file instead :)
-        this.state.details.username = "Anthony";
-        this.state.details.password = "food";
+        this.setState({
+            details: {
+                username: "Anthony",
+                password: "food"
+            }
+
+        });
+        //this.state.details.username = "Anthony";
+        //this.state.details.password = "food";
         this.login();
     }
 
